af495b80926046ef8bcb1214a10c59dd32550e54
3PT <3PT@B208-12>
2026-09-25 12:02:48 +0300

    Country Update

--- src/operations/UpdateOperation.java
+++ src/operations/UpdateOperation.java
@@ -51,37 +51,55 @@ public class UpdateOperation {
             System.out.println("City atjauninats: " + rows + " rindas");
         }
     }
-    public void updateCountry(Connection con)throws SQLException{
-    	 System.out.println("Kuru valstu labot? (Noradi ID)");
-         int vid = Integer.parseInt(scan.nextLine().trim());
-
-         System.out.println("Noradi valsta nosaukumu");
-         String vname = scan.nextLine().trim();
-
-         System.out.println("Noradi valsts kodu (3 simboli)");
-         String code2 = scan.nextLine().trim().toUpperCase();
-
-         System.out.println("Noradi apgabalu");
-         String district = scan.nextLine().trim();
-         
-         System.out.println("Noradi Regionu");
-         String region = scan.nextLine().trim();
-
-         System.out.println("Noradi iedzivotaju skaitu");
-         int population = Integer.parseInt(scan.nextLine().trim());
-         
-         String sql = "UPDATE city SET Name = ?, Code2 = ?, District = ?, Population = ?,Region=? WHERE ID = ?";
-
-         try (PreparedStatement ps = con.prepareStatement(sql)) {
-             ps.setString(1, vname);
-             ps.setString(2, code2);
-             ps.setString(3, district);
-             ps.setInt(4, population);
-             ps.setInt(5, vid);
-             ps.setString(6, region);;
-
-             int rows = ps.executeUpdate();
-             System.out.println("City atjauninats: " + rows + " rindas");
-         }
-    }
+private void updateCountry(Connection con)throws SQLException{
+		
+		System.out.println("Ievadi Valsts codu (3 simboli)");
+		String code = scan.nextLine();
+		System.out.println("Ievadi valsts nosaukumu");
+		String vname = scan.nextLine().trim().toUpperCase();
+		System.out.println("Noradi kontinentu");
+		String vcontinent = scan.nextLine();
+		System.out.println("Noradi regionu ");
+		String vregion = scan.nextLine();	
+		System.out.println("Ievadi virsmas laukumu");
+		int area = Integer.parseInt(scan.nextLine().trim());
+		System.out.println("Ievadi Neatkarību gadu");
+		int vgags = Integer.parseInt(scan.nextLine().trim());
+		System.out.println("Ievadi iedzivotaju skaitu");
+		int vpop = Integer.parseInt(scan.nextLine().trim());
+		System.out.println("Ievadi Videjo dzivibas garumu");
+		double vdzgar = scan.nextDouble();
+		scan.nextLine();
+		System.out.println("Ievadi lokalu nosaukumu");
+		String LName = scan.nextLine().trim().toUpperCase();
+		System.out.println("Ievadi Valdības veidlapa");
+		String GName = scan.nextLine();
+		System.out.println("Ievadi Valsts vadītāju");
+		String VName = scan.nextLine();
+		System.out.println("Ievadi Kapitālu");
+		int kapitals = Integer.parseInt(scan.nextLine().trim());
+		System.out.println("Ievadi Valsts kodu2 (2 simboli)");
+		String code2 = scan.nextLine();
+		
+		String sql=
+				"UPDATE country SET Code = ?, Name = ?, Continent = ?, Region = ?, SurfaceArea = ?, IndepYear = ?, Population = ?,LifeExpectancy = ?, LocalName = ?, GovernmentForm = ?, HeadOfState = ?, Capital = ? WHERE Code2 = ?";
+		try(PreparedStatement ps = con.prepareStatement(sql)){
+			ps.setString(1,code);
+			ps.setString(2, vname);
+			ps.setString(3, vcontinent);
+			ps.setString(4, vregion);
+			ps.setInt(5, area);
+			ps.setInt(6, vgags);
+			ps.setInt(7, vpop);
+			ps.setDouble(8, vdzgar);
+			ps.setString(9, LName);
+			ps.setString(10, GName);
+			ps.setString(11, VName);
+			ps.setInt(12, kapitals);
+			ps.setString(13, code2);
+			int rows = ps.executeUpdate();
+			System.out.println("Country tabula ievetotas "+ rows + " rindas");
+		}
+		
+	}
 }
