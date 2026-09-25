687215951cacdb75841c6383c32a573186acf953
3PT <3PT@B208-12>
2026-09-25 12:15:32 +0300

    DELETE CONTRY AND COUNTRY LANGUAGE

--- src/operations/DeleteOperation.java
+++ src/operations/DeleteOperation.java
@@ -11,8 +11,8 @@ public void delete(Connection con, String table) {
 	try {
 		switch(table) {
 		case "city" -> deleteCity(con);
-		//case "country" -> deleteCountry(con);
-		//case "countrylanguage" -> deleteClanguage(con);
+		case "country" -> deleteCountry(con);
+		case "countrylanguage" -> deletelanguage(con);
 		default -> System.out.println("Neatbilstita tabula " + table);
 		}
 		
@@ -36,5 +36,29 @@ private void deleteCity(Connection con)throws SQLException{
 			                            
 		}
 }
+private void deleteCountry(Connection con)throws SQLException{
+	System.out.println("Noradi Countyr code kuru dzest (3 simboli)");
+		String Code = scan.nextLine();
+		
+		String sql = "DELETE FROM country WHERE Code = ?";
+		try(PreparedStatement ps = con.prepareStatement(sql)){
+			ps.setString(1, Code);
+			int rows = ps.executeUpdate();
+			System.out.println("country tabula dzesti "+rows+" ieraksti");
+			                            
+		}
+}
 
+private void deletelanguage(Connection con)throws SQLException{
+	System.out.println("Noradi Countyr code kuru dzest (3 simboli)");
+		String Code = scan.nextLine();
+		
+		String sql = "DELETE FROM CountryLanguage WHERE CountryCode = ?";
+		try(PreparedStatement ps = con.prepareStatement(sql)){
+			ps.setString(1, Code);
+			int rows = ps.executeUpdate();
+			System.out.println("countryLanguage tabula dzesti "+rows+" ieraksti");
+			                            
+		}
+}
 }
